b33f4bf23e748a902dfd2b3b5b8ab58eb08d2a6f
Prottoy Kumar Prodhan Joy <80445079+ProttoyKumar@users.noreply.github.com>
2026-05-14 02:14:56 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException, status, Query
+import json
 
 app=FastAPI()
 
-foods={'Indian':['Bhatura','Chaat'],
-       'Bengali':['Mutton Curry','Macher Jhol'],
-       'Chinese':['Dumplings','Tofu']}
+def load_data():
+    with open('patients_info.json','r') as f:
+        data = json.load(f)
+    return data
+        
+@app.get('/')
+def hello():
+    return {'message':'Patient Management System API.'}
 
-@app.get('/get_items/{cuisine}') 
-async def get_items(cuisine):
-    return foods.get(cuisine)
+@app.get('/view')
+def view():
+    data=load_data()
+    return data
 
-discount={1:'10%', 2:'15%', 3:'20%'}
+@app.get('/view_patient/{ID}')
+def view_patient(ID: str = Path(..., description='Patient Id', example='PAT001')):
+    data = load_data()
+    
+    # Check if the patient ID actually exists
+    if ID not in data:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail=f"Patient with ID {ID} not found"
+        )
+    return data[ID]
 
-@app.get('/cupon/{code}')
-async def cupon_code(code: int):
-    return discount.get(code)
+@app.get('/sort')
+def sort_patients(sort_by: str = Query(...,description='sort data by height, weight or BMI.'), order: str = Query('asc',descrition='Sort order: asc or desc.')):
+
+    data = load_data()
+    sort_order = True if order == 'desc' else False
+    sorted_data = sorted(data.values(), key= lambda x:x[sort_by], reverse=sort_order)
+    return sorted_data
+    
